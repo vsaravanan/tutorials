4a646accefa27f438dfac96122a3f801f3101483
viswa <saravanan.resume@gmail.com>
2023-10-17 15:15:45 +0800

    added createApi

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { api } from 'features/api/apislice'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import { applyMiddleware } from '@reduxjs/toolkit'
 import counterReducer from 'redux/reducers/counterSlice'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+const middlewares = [thunk]
+
+const environment = process.env.NODE_ENV
+console.log(environment)
+
+if (environment === 'development') {
+  middlewares.push(logger)
+}
+middlewares.push(api.middleware)
+
+const initalState = {}
+
+export const store = configureStore(
+  {
+    reducer: {
+      [api.reducerPath]: api.reducer,
+      counter: counterReducer,
+    },
+    middleware: middlewares,
   },
-})
+  initalState,
+  // composeWithDevTools(applyMiddleware(...middlewares)),
+)
