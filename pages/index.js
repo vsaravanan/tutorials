9c6e1a9b9cc25c682fee145309274fa22fcfa540
viswa <saravanan.resume@gmail.com>
2023-10-18 18:19:09 +0800

    removed counter

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
-// import TreeView from '@/components/TreeView/TreeView'
-
-import { Counter } from '@/components/counter/counter'
-
 const Index = () => {
-  return (
-    <>
-      <Counter></Counter>
-    </>
-  )
+  return <>{/* <div></div> */}</>
 }
 
 export default Index
--- redux/store.js
+++ redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 // import { api } from 'features/api/apislice'
 // import { composeWithDevTools } from 'redux-devtools-extension'
 // import { applyMiddleware } from '@reduxjs/toolkit'
-import counterReducer from 'redux/reducers/counterSlice'
+// import counterReducer from 'redux/reducers/counterSlice'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
@@ -22,7 +22,7 @@ export const store = configureStore(
   {
     reducer: {
       // [api.reducerPath]: api.reducer,
-      counter: counterReducer,
+      // counter: counterReducer,
     },
     middleware: middlewares,
   },
