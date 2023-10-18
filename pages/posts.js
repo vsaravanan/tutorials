ae0183174b2b931535f93b6ca7c3d3d2a04b4de7
viswa <saravanan.resume@gmail.com>
2023-10-19 01:55:33 +0800

    added isLoading, isFetching

--- pages/posts.js
+++ pages/posts.js
@@ -1,8 +1,16 @@
 import { useFetchPostQuery } from 'features/api/apiPosts'
 
 export default function Posts() {
-  const { data: posts } = useFetchPostQuery()
+  const { data: posts = [], isLoading, isFetching, isError, error } = useFetchPostQuery()
 
+  if (isLoading || isFetching) {
+    return <div>loading...</div>
+  }
+
+  if (isError) {
+    console.log({ error })
+    return <div>{error.status}</div>
+  }
   return (
     <ul>
       {posts?.map(post => (
